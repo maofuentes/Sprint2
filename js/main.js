fe5bfe155b37fbd55a7ab0bf865154d40199519b
Franco Valdés <franco.v.navarro@gmail.com>
2023-03-31 22:17:29 -0300

    "Formulario de ingreso para nuevos usuarios 31/03 22:18"

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@ function ingreso() {
         alert("Usuario y/o clave Incorrecta");
     }
 }
+
+function registro() {
+    //Esta función sólo revisa si todas las formas presentes en el registro de
+    //usuario fueron rellenadas y, si todas tienen un valor, entonces envía a la página
+    //de privado/index.html
+
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var rut = document.getElementById("rut").value;
+    var prevision = document.getElementById("prevision").value;
+    var clave = document.getElementById("clave").value;
+
+    //Si todos los datos fueron rellenados, envía a producto.html
+    //Si falta al menos alguna forma, muestra un mensaje
+
+    if ((nombre === "") || (apellido === "") || rut === "" || prevision === "" || clave === "") {
+        alert("Uno o más elementos no han sido ingresados");
+    } else {
+        window.location.href = "privado/index.html"
+    }
+}
