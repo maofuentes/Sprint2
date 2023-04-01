6e09af9ae2b3ce4a40b9b5402aed6da15496defb
maofuentes <mfuentesoliva@revolt.cl>
2023-03-31 21:31:34 -0300

    Desarrollo Marcelo 21:31

--- privado/privado.js
+++ privado/privado.js
@@ -1,10 +1,19 @@
 const ctx = document.getElementById('myChart');
 const ctx2 = document.getElementById('myChart2');
+const ctx3 = document.getElementById('myChart3');
+const ctx4 = document.getElementById('myChart4');
+
+$( function() {
+  $( "#campofecha" ).datepicker({
+    numberOfMonths: 2,
+    showButtonPanel: true
+  });
+} );
 
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Hemograma, perfil lipídico', 'Colesterol', 'Triglicéridos', 'Orina', 'Perfil bioquímico', 'Coagulación'],
+    labels: ['Hemograma', 'perfil lipídico', 'Colesterol', 'Triglicéridos', 'Orina', 'Perfil bioquímico', 'Coagulación'],
     datasets: [{
       label: 'Cantidad de Examenes realizados en Marzo',
       data: [12, 19, 3, 5, 2, 13],
@@ -23,10 +32,48 @@ new Chart(ctx, {
 new Chart(ctx2, {
   type: 'bar',
   data: {
-    labels: ['Hemograma, perfil lipídico', 'Colesterol', 'Triglicéridos', 'Orina', 'Perfil bioquímico', 'Coagulación'],
+    labels: ['Hemograma', 'Perfil lipídico', 'Colesterol', 'Triglicéridos', 'Orina', 'Perfil bioquímico', 'Coagulación'],
     datasets: [{
       label: 'Cantidad de Examenes realizados en Marzo',
       data: [18, 19, 3, 5, 2, 13],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx3, {
+  type: 'line',
+  data: {
+    labels: ['Hemograma', 'perfil lipídico', 'Colesterol', 'Triglicéridos', 'Orina', 'Perfil bioquímico', 'Coagulación'],
+    datasets: [{
+      label: 'Cantidad de Examenes realizados en Marzo',
+      data: [18, 19, 3, 5, 2, 13],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx4, {
+  type: 'line',
+  data: {
+    labels: ['Hemograma', 'perfil lipídico', 'Colesterol', 'Triglicéridos', 'Orina', 'Perfil bioquímico', 'Coagulación'],
+    datasets: [{
+      label: 'Cantidad de Examenes realizados en Marzo',
+      data: [3, 19, 23, 6, 18, 13],
       borderWidth: 1
     }]
   },
